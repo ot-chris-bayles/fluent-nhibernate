d45c548edc9503b07a2cf0fb923971af032afb71
James Gregory <james@jagregory.com>
2010-10-17 15:10:54 +0100

    Migrated some persistencemodel tests over to specs

---
+++ src/FluentNHibernate.Specs/PersistenceModel/PersistenceModelSpecs.Joins.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using FluentNHibernate.Cfg.Db;
+using FluentNHibernate.Mapping;
+using FluentNHibernate.Visitors;
+using Machine.Specifications;
+using NHibernate.Cfg;
+
+namespace FluentNHibernate.Specs.PersistenceModel
+{
+    public class when_the_persistence_model_is_told_to_build_the_mappings_for_a_class_with_a_join
+    {
+        Establish context = () =>
+        {
+            cfg = new Configuration();
+
+            SQLiteConfiguration.Standard.InMemory()
+                .ConfigureProperties(cfg);
+
+            model = new FluentNHibernate.PersistenceModel();
+            
+            var class_map = new ClassMap<Target>();
+            class_map.Id(x => x.Id);
+            class_map.Join("other", m => m.Map(x => x.Property));
+            
+            model.Add(class_map);
+        };
+
+        Because of = () =>
+            cfg.ConfigureWith(model);
+
+        It shouldnt_duplicate_join_mapping = () =>
+            cfg.ClassMappings.First()
+                .JoinClosureIterator.Count().ShouldEqual(1);
+        
+        static FluentNHibernate.PersistenceModel model;
+        static Configuration cfg;
+
+        class Target
+        {
+            public int Id { get; set; }
+            public string Property { get; set; }
+        }
+    }
+}
---
+++ src/FluentNHibernate.Specs/PersistenceModel/PersistenceModelSpecs.SubclassVisitor.cs
@@ -0,0 +1,267 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentNHibernate.Mapping;
+using FluentNHibernate.MappingModel.ClassBased;
+using Machine.Specifications;
+
+namespace FluentNHibernate.Specs.PersistenceModel
+{
+    public class when_pairing_subclasses_of_interfaces_with_direct_implementations : SubclassPairingSpecBase
+    {
+        Establish context = () =>
+        {
+            model = new FluentNHibernate.PersistenceModel();
+            model.Add(new FooMap());
+            model.Add(new StringFooMap());
+            model.Add(new StandAloneMap());
+        };
+
+        Because of = () =>
+            mapping = model.BuildMappings()
+                .Classes.Single();
+
+        // The Parent is the IFoo interface the desired results 
+        // of this test is the inclusion of the Foo<T> through the
+        // GenericFooMap<T> subclass mapping.
+        It should_add_subclass_that_implements_the_parent_interface = () =>
+        {
+            mapping.Subclasses.Count().ShouldEqual(1);
+            mapping.Subclasses.Select(x => x.Type).ShouldContain(typeof(Foo<string>));
+        };
+
+        // The Parent is the IFoo interface the desired results 
+        // of this test is the exclusion of the StandAlone class 
+        // since it does not implement the interface.
+        It should_not_add_subclass_that_doesnt_implement_the_parent_interface = () =>
+            mapping.Subclasses.Select(x => x.Type).ShouldNotContain(typeof(StandAlone));
+
+        static ClassMapping mapping;
+    }
+
+    public class when_pairing_subclasses_of_interfaces_with_abstract_base : SubclassPairingSpecBase
+    {
+        Establish context = () =>
+        {
+            model = new FluentNHibernate.PersistenceModel();
+            model.Add(new FooMap());
+            model.Add(new StringFooMap());
+            model.Add(new StandAloneMap());
+            model.Add(new BaseImplMap());
+        };
+
+        Because of = () =>
+            mapping = model.BuildMappings()
+                .Classes.Single();
+
+        // The Parent is the IFoo interface the desired results 
+        // of this test is the exclusion of the StandAlone class 
+        // since it does not implement the interface.
+        It should_not_add_subclass_that_doesnt_implement_the_parent_interface = () =>
+            mapping.Subclasses.Select(x => x.Type).ShouldNotContain(typeof(StandAlone));
+
+        // The Parent is the IFoo interface the desired results 
+        // of this test is the inclusion of the BaseImpl class and 
+        // the exclusion of the Foo<T> class since it implements 
+        // the BaseImpl class which already implements FooBase.
+        It Should_not_add_subclassmap_that_implements_a_subclass_of_the_parent_interface = () =>
+        {
+            mapping.Subclasses.Count().ShouldEqual(1);
+            mapping.Subclasses.Where(x => x.Type.Equals(typeof(BaseImpl))).Count().ShouldEqual(1);
+        };
+
+        static ClassMapping mapping;
+    }
+
+    public class when_pairing_subclasses_with_explicit_extends : SubclassPairingSpecBase
+    {
+        Establish context = () =>
+        {
+            model = new FluentNHibernate.PersistenceModel();
+            model.Add(new ExtendsParentMap());
+            model.Add(new ExtendsChildMap());
+        };
+
+        Because of = () =>
+            mapping = model.BuildMappings()
+                .Classes.Single();
+
+        It should_add_explicit_extend_subclasses_to_their_parent = () =>
+        {
+            mapping.Subclasses.Count().ShouldEqual(1);
+            mapping.Subclasses.Select(x => x.Type).ShouldContain(typeof(ExtendsChild));
+        };
+        
+        static ClassMapping mapping;
+    }
+
+    public class when_pairing_subclasses_with_a_abstract_base : SubclassPairingSpecBase
+    {
+        Establish context = () =>
+        {
+            model = new FluentNHibernate.PersistenceModel();
+            model.Add(new StringFooMap());
+            model.Add(new BaseMap());
+            model.Add(new StandAloneMap());
+        };
+
+        Because of = () =>
+            mapping = model.BuildMappings().Classes.Single();
+
+        // The Parent is the FooBase class the desired results 
+        // of this test is the inclusion of the Foo<T> through the
+        // GenericFooMap<T> subclass mapping.
+        It should_add_subclass_that_implements_the_parent_base = () =>
+        {
+            mapping.Subclasses.Count().ShouldEqual(1);
+            mapping.Subclasses.Select(x => x.Type).ShouldContain(typeof(Foo<string>));
+        };
+
+        // The Parent is the FooBase class the desired results 
+        // of this test is the exclusion of the StandAlone class 
+        // since it does not implement the interface.
+        It should_not_add_subclassmap_that_does_not_implement_parent_base = () =>
+            mapping.Subclasses.Select(x => x.Type).ShouldNotContain(typeof(StandAlone));
+
+        static ClassMapping mapping;
+    }
+    
+    public class when_pairing_subclasses_with_an_intermediary_abstract_base : SubclassPairingSpecBase
+    {
+        Establish context = () =>
+        {
+            model = new FluentNHibernate.PersistenceModel();
+            model.Add(new BaseMap());
+            model.Add(new StandAloneMap());
+            model.Add(new BaseImplMap());
+        };
+
+        Because of = () =>
+            mapping = model.BuildMappings().Classes.Single();
+
+        // The Parent is the FooBase class the desired results 
+        // of this test is the exclusion of the StandAlone class 
+        // since it does not implement the interface.
+        It should_not_add_subclassmap_that_does_not_implement_parent_base = () =>
+            mapping.Subclasses.Select(x => x.Type).ShouldNotContain(typeof(StandAlone));
+
+        // The Parent is the FooBase class the desired results 
+        // of this test is the inclusion of the BaseImpl class and 
+        // the exclusion of the Foo<T> class since it implements 
+        // the BaseImpl class which already implements FooBase.
+        It should_not_add_subclassmap_that_implements_a_subclass_of_the_parent_base = () =>
+        {
+            mapping.Subclasses.Count().ShouldEqual(1);
+            mapping.Subclasses.Select(x => x.Type).ShouldContain(typeof(BaseImpl));
+        };
+
+        static ClassMapping mapping;
+    }
+
+    public class when_pairing_subclasses_with_unioning_enabled : SubclassPairingSpecBase
+    {
+        Establish context = () =>
+        {
+            model = new FluentNHibernate.PersistenceModel();
+            model.Add(new StringFooMap());
+            model.Add(new UnionBaseMap());
+        };
+
+        Because of = () =>
+            mapping = model.BuildMappings().Classes.Single();
+
+        It Should_choose_UnionSubclass_when_the_class_mapping_IsUnionSubclass_is_true = () =>
+            mapping.Subclasses.All(x => x.SubclassType == SubclassType.UnionSubclass).ShouldBeTrue();
+
+        static ClassMapping mapping;
+    }
+
+    public abstract class SubclassPairingSpecBase
+    {
+        protected static FluentNHibernate.PersistenceModel model;
+        protected static IEnumerable<ClassMapping> classes;
+
+        protected interface IFoo
+        {
+            int Id { get; set; }
+        }
+
+        protected class Base : IFoo
+        {
+            public int Id { get; set; }
+        }
+
+        protected abstract class BaseImpl : Base
+        { }
+
+        protected class Foo<T> : BaseImpl, IFoo
+        { }
+
+        protected class FooMap : ClassMap<IFoo>
+        {
+            public FooMap()
+            {
+                Id(x => x.Id);
+            }
+        }
+
+        protected class BaseMap : ClassMap<Base>
+        {
+            public BaseMap()
+            {
+                Id(x => x.Id);
+            }
+        }
+
+        protected class UnionBaseMap : ClassMap<Base>
+        {
+            public UnionBaseMap()
+            {
+                Id(x => x.Id);
+                UseUnionSubclassForInheritanceMapping();
+            }
+        }
+
+        protected class BaseImplMap : SubclassMap<BaseImpl>
+        { }
+
+        protected abstract class GenericFooMap<T> : SubclassMap<Foo<T>>
+        { }
+
+        protected class StringFooMap : GenericFooMap<string>
+        { }
+
+
+        private interface IStand
+        { }
+
+        protected class StandAlone : IStand
+        { }
+
+        protected class StandAloneMap : SubclassMap<StandAlone>
+        { }
+
+        protected class ExtendsParent
+        {
+            public int Id { get; set; }
+        }
+
+        protected class ExtendsChild
+        { }
+
+        protected class ExtendsParentMap : ClassMap<ExtendsParent>
+        {
+            public ExtendsParentMap()
+            {
+                Id(x => x.Id);
+            }
+        }
+
+        protected class ExtendsChildMap : SubclassMap<ExtendsChild>
+        {
+            public ExtendsChildMap()
+            {
+                Extends<ExtendsParent>();
+            }
+        }
+    }
+}
